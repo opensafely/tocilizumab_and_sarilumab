04ec68e1cb7fb1c7e6b2cf780fb3c6e55802af77
lshbz1 <98110657+lshbz1@users.noreply.github.com>
2026-01-24 23:38:36 +0800

    Update plot.R

--- analysis/plot.R
+++ analysis/plot.R
@@ -6,9 +6,24 @@ library(tidyverse)
 
 df <- read_dta("./output/main.dta")
 
+if(is.numeric(df$drug)) {
+  df$drug <- as.factor(df$drug)
+}
+
+df <- df %>%
+  mutate(
+    end_date = as.Date(end_date),
+    start_date = as.Date(start_date),
+    time  = as.numeric(end_date - start_date),
+    event = failure == 1,
+    drug = factor(drug, 
+                  levels = c(0, 1), 
+                  labels = c("tocilizumab", "sarilumab"))
+  )
 
 
 svg("./output/km_plot.svg", width = 8, height = 6)
+
 
 
 dev.off()
