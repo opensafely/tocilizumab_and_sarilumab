750aa81e66ac632211654443a7f213a9bb9e0116
lshbz1 <98110657+lshbz1@users.noreply.github.com>
2026-01-24 23:45:16 +0800

    Update plot.R

--- analysis/plot.R
+++ analysis/plot.R
@@ -22,6 +22,27 @@ df <- df %>%
   )
 
 
+
+#by drug #
+km_raw <- survfit(
+  Surv(time, event ) ~ drug,
+  data = df
+)
+threshold <- 10
+
+km_min10 <- broom::tidy(km_raw) |>
+  group_by(strata) |>
+  mutate(
+    cml_event = cumsum(n.event),
+    cml_event = floor(cml_event / threshold) * threshold,
+    n.event   = c(cml_event[1], diff(cml_event))
+  ) |>
+  mutate(
+    n.risk = max(n.risk) - lag(cumsum(n.event + n.censor), default = 0),
+    surv   = cumprod(1 - n.event / n.risk)
+  ) |>
+  ungroup()
+
 svg("./output/km_plot.svg", width = 8, height = 6)
 
 
