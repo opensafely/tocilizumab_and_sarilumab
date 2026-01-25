bc3054e15ea7f995973c1cf394db725ae868534c
lshbz1 <98110657+lshbz1@users.noreply.github.com>
2026-01-25 15:21:47 +0800

    Update plot.R

--- analysis/plot.R
+++ analysis/plot.R
@@ -30,9 +30,36 @@ km_raw <- survfit(
 )
 threshold <- 10
 
+km_sum <- summary(km_raw)
+
+km_df <- data.frame(
+  strata   = km_sum$strata,
+  time     = km_sum$time,
+  n.risk   = km_sum$n.risk,
+  n.event  = km_sum$n.event,
+  n.censor = km_sum$n.censor,
+  surv     = km_sum$surv
+)
+
+
+km_min10 <- km_df |>
+  group_by(strata) |>
+  mutate(
+    N = max(n.risk),
+    
+    cml_event  = floor(cumsum(n.event)  / threshold) * threshold,
+    cml_censor = floor(cumsum(n.censor) / threshold) * threshold,
+    
+    n.event  = c(cml_event[1],  diff(cml_event)),
+    n.censor = c(cml_censor[1], diff(cml_censor)),
+    
+    n.risk = N - lag(cumsum(n.event + n.censor), default = 0),
+    
+    surv = cumprod(1 - n.event / n.risk)
+  ) |>
+  ungroup()
+
 svg("./output/km_plot.svg", width = 8, height = 6)
-
-
 
 dev.off()
 
